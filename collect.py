3f4c8c88f6de5cb8cf66580ad73b81aac86304bf
oilime <wccemilio@gmail.com>
2017-03-06 18:43:17 +0800

    data collect

--- collect.py
+++ collect.py
@@ -3,17 +3,22 @@ from __future__ import division
 import requests
 from bs4 import BeautifulSoup
 import math
-import os
+import sys
+reload(sys)
 
+sys.setdefaultencoding('utf-8')
 
 __metaclass__ = type
+
+
 class Good:
-    def __init__(self, name, price, group, img, goodtype):
+    def __init__(self, name, price, group, img, goodtype, weight=0):
         self.__name = name
         self.__price = price
         self.__group = group
         self.__img = img
         self.__type = goodtype
+        self.__weight = weight
 
     def get_good_name(self):
         return self.__name
@@ -30,7 +35,11 @@ class Good:
     def get_good_img(self):
         return self.__img
 
+    def get_good_weight(self):
+        return self.__weight
 
+
+f = open('data.txt', 'w')
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
 url = 'https://shop.48.cn/Goods'
 start_html = requests.get(url, headers=headers).content
@@ -38,9 +47,10 @@ soup = BeautifulSoup(start_html, "lxml")
 shop = soup.find('div', class_='body-content').find('span', class_='pink')
 num = int(shop.get_text())
 pagecount = 80
-pagenum = 1
+pagenum = 0
+data = []
 
-while pagenum <= math.ceil(num/pagecount):
+while pagenum < math.ceil(num/pagecount):
     url = 'https://shop.48.cn/Goods?totalCount={}&brand_id=-1&pageNum={}&numPerPage={}'.format(num, pagenum, pagecount)
     shop_html = requests.get(url, headers=headers).content
     soup = BeautifulSoup(shop_html, 'lxml')
@@ -51,9 +61,18 @@ while pagenum <= math.ceil(num/pagecount):
         good_html = requests.get(url, headers=headers).content
         soup = BeautifulSoup(good_html, 'lxml')
         name = soup.find('li', class_='i_tit').get_text()
-        print name
-        price = soup.find('li', class_='i_jg')
-        print price
+        price = soup.find('li', class_='i_jg').find('span')
+        group = soup.find('li', class_='i_num').find_all('span')[1].get_text()
+        goodtype = soup.find('li', class_='i_num').find_all('span')[3].get_text()
+        img = soup.find('div', class_='pic_m').find('a')['href']
+
+        print name.encode('utf-8')
+        # data.append(Good(name, price, group, img, goodtype))
+        k = ''.join(name.encode('utf-8')) #.join(' ').join(price.encode('utf-8')).join(' ').join(group.encode('utf-8')).join(' ').join(goodtype.encode('utf-8'))
+        f.write(k + '\n')
+        f.flush()
     pagenum += 1
+f.close()
+print 'over!!!'
 
 
